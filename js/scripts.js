d52da2e007fab08c9ea0c0cb8593614ff6f11d42
Deibys <atom32@tutanota.com>
2022-06-04 00:42:52 -0400

    frontend completed

---
+++ js/scripts.js
@@ -0,0 +1,69 @@
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 72,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+    const contactForm = document.querySelector('form.contact');
+
+    let name = document.querySelector('input[type="text"]').value
+    let email = document.querySelector('input[type="email"]').value
+    let message = document.querySelector('textarea').value
+
+    const handlingForm = function (e) {
+        e.preventDefault();
+
+        const data = {name, email, message}
+
+        fetch('https://contact-backed.herokuapp.com/', {method: 'POST', body: JSON.stringify(data)})
+            .then(respose => response.json())
+            .then(data => {
+                contactForm.reset();
+            });
+    }
+
+    contactForm.addEventListener('submit', handlingForm)
+
+});
